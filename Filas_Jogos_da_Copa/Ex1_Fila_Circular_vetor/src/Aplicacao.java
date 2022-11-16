f1ac67523b5378f4dac9c69acfb95ddaec234fe2
jgapadua <joaogabrielpadua@gmail.com>
2022-11-16 14:35:45 -0300

    att

--- Filas_Jogos_da_Copa/Ex1_Fila_Circular_vetor/src/Aplicacao.java
+++ Filas_Jogos_da_Copa/Ex1_Fila_Circular_vetor/src/Aplicacao.java
@@ -1,11 +1,10 @@
 import java.io.*;
-
 class ArquivoTextoLeitura {
 
 	private BufferedReader entrada;
-	
-	ArquivoTextoLeitura(String nomeArquivo) {	
-		
+
+	ArquivoTextoLeitura(String nomeArquivo) {
+
 		try {
 			entrada = new BufferedReader(new InputStreamReader(new FileInputStream(nomeArquivo), "UTF-8"));
 		} catch (UnsupportedEncodingException excecao) {
@@ -14,38 +13,147 @@ class ArquivoTextoLeitura {
 			System.out.println("Arquivo nao encontrado");
 		}
 	}
-	
+
 	public void fecharArquivo() {
-		
+
 		try {
 			entrada.close();
-		}
-		catch (IOException excecao) {
-			System.out.println("Erro no fechamento do arquivo de leitura: " + excecao);	
+		} catch (IOException excecao) {
+			System.out.println("Erro no fechamento do arquivo de leitura: " + excecao);
 		}
 	}
-	
+
 	@SuppressWarnings("finally")
 	public String ler() {
-		
+
 		String textoEntrada = null;
-		
+
 		try {
 			textoEntrada = entrada.readLine();
-		}
-		catch (EOFException excecao) { //Excecao de final de arquivo.
+		} catch (EOFException excecao) { // Excecao de final de arquivo.
 			textoEntrada = null;
-		}
-		catch (IOException excecao) {
+		} catch (IOException excecao) {
 			System.out.println("Erro de leitura: " + excecao);
 			textoEntrada = null;
-		}
-		finally {
+		} finally {
 			return textoEntrada;
 		}
 	}
 }
 
+class Fila {
+
+	private Jogo[] fila;
+	private int frente;
+	private int tras;
+	private int tamanho;
+	
+	public Fila(int tamanho) {
+		
+		fila = new Jogo[tamanho];
+		frente = 0;
+		tras = 0;
+		this.tamanho = tamanho;
+	}
+	
+	public Fila() {
+		
+		fila = new Jogo[101];
+		frente = 0;
+		tras = 0;
+		this.tamanho = 101;
+	}
+	
+	public boolean filaVazia() {
+	
+		boolean resp;
+		
+		if (frente == tras)
+			resp = true;
+		else
+			resp = false;
+		
+		return resp;
+	}
+	
+	public boolean filaCheia() {
+	
+		boolean resp;
+		
+		if (((tras + 1) % tamanho) == (frente % tamanho))
+			resp = true;
+		else
+			resp = false;
+		
+		return resp;
+	}
+	
+	public void enfileirar(Jogo novo) throws Exception{
+		
+		int posicao;
+		
+		if (!filaCheia()) {
+			posicao = tras % tamanho;
+			fila[posicao] = novo;
+			tras++;
+			MyIO.println((int)obterMediaGols());
+		} else {
+			desenfileirar();
+			enfileirar(novo);
+		}
+
+	}
+	
+	public Jogo desenfileirar() throws Exception{
+		
+		Jogo desenfileirado;
+		int posicao;
+		
+		if (!filaVazia()) {
+			posicao = frente % tamanho;
+			desenfileirado = fila[posicao];
+			frente++;
+			return desenfileirado;
+		} else
+			throw new Exception("Não foi possível desenfileirar nenhum elemento: a fila está vazia!");
+	}
+	public void mostrar(Jogo desenfileirado) {
+
+		System.out.print("(D) ");
+		desenfileirado.imprimir();
+
+	}
+	public void mostrarJogos() throws Exception{
+		
+		int posicao;
+		int sequencia = 0;
+		if (!filaVazia()) {
+			for (int i = frente; i < tras; i++) {
+				posicao = i % tamanho;
+				System.out.print("[" + sequencia + "]");
+				fila[posicao].imprimir();
+				sequencia++;
+			}
+		} else
+			throw new Exception ("Não foi possível mostrar o conteúdo da fila: a fila está vazia!");
+	}
+	
+	public double obterMediaGols() {
+		double mediaTotal = 0;
+		int posicao;
+		int tamanhoArr = 0;
+		
+		if (!filaVazia()) {
+			for (int i = frente; i < tras; i++) {
+				tamanhoArr++;
+				posicao = i % tamanho;
+				mediaTotal += fila[posicao].getPlacarSelecao1() + fila[posicao].getPlacarSelecao2();
+			}
+			mediaTotal = mediaTotal / tamanhoArr;
+		}
+		return Math.round(mediaTotal);
+	}
+}
 class Jogo{
 	//declaração dos atributos
 private int dia;
@@ -148,19 +256,18 @@ return cloneJogo;
 
 // método para ler arquivo
 public  static Jogo[] lerArquivo() {
-	String nomeArquivo="src/partidas.txt";
-/*   String nomeArquivo="/tmp/partidas.txt";  */
+/*   String nomeArquivo="src/partidas.txt"; */
+  String nomeArquivo="/tmp/partidas.txt";
   ArquivoTextoLeitura arquivoLeitura = new ArquivoTextoLeitura(nomeArquivo);
   
   // Preenchendo um vetor de objetos com os dados do arquivo
-	int quantidade = 1000;
+	int quantidade = 900;
   Jogo[] vetor = new Jogo[quantidade];
   int i = 0;
   String str = arquivoLeitura.ler();
   while (str != null) {
     vetor[i++] = new Jogo(str);
     str = arquivoLeitura.ler();
-		quantidade++;
   }
   arquivoLeitura.fecharArquivo();
   return vetor;
@@ -171,182 +278,56 @@ public void imprimir() {
 	System.out.println("[COPA " + this.ano + "] [" + this.etapa + "] [" + this.dia + "/" + this.mes + "] ["+ this.selecao1 + " (" + this.placarSelecao1 + ") x (" + this.placarSelecao2 + ") " + this.selecao2 + "] [" + this.local + "]");
 	}
 	}
-
-class Fila {
-
-	private Jogo fila[];
-	private int frente;
-	private int tras;
-	private int tamanho;
-	
-	public Fila(int tamanho) {
-		
-		fila = new Jogo[tamanho];
-		frente = 0;
-		tras = 0;
-		this.tamanho = tamanho;
-	}
-	
-	public Fila() {
-		
-		fila = new Jogo[101];
-		frente = 0;
-		tras = 0;
-		this.tamanho = 101;
-	}
-	
-	public boolean filaVazia() {
-	
-		boolean resp;
-		
-		if (frente == tras)
-			resp = true;
-		else
-			resp = false;
-		
-		return resp;
-	}
-	
-	public boolean filaCheia() {
-	
-		boolean resp;
-		
-		if (((tras + 1) % tamanho) == (frente % tamanho))
-			resp = true;
-		else
-			resp = false;
-		
-		return resp;
-	}
-	
-	public boolean enfileirar(Jogo novo) throws Exception{
-		
-		int posicao;
-		
-		if (!filaCheia()) {
-			posicao = tras % tamanho;
-			fila[posicao] = novo;
-			tras++;
-			System.out.println((int)obterMediaGols());
-			return true;
-		} else {
-			throw new Exception("Não foi possível enfileirar: a fila está cheia!");
-
-		}
-
-	}
-	
-	public Jogo desenfileirar() throws Exception{
-		
-		Jogo desenfileirado;
-		int posicao;
-		
-		if (!filaVazia()) {
-			posicao = frente % tamanho;
-			desenfileirado = fila[posicao];
-			frente++;
-			return desenfileirado;
-		} else
-			throw new Exception("Não foi possível desenfileirar: a fila está vazia!");
-	}
-	public void mostrar(Jogo desenfileirado) {
-
-		System.out.print("(D) ");
-		desenfileirado.imprimir();
-
-	}
-	public void mostrarJogo(){
-		int posicao;
-		int sequencia = 0;
-		if (!filaVazia()) {
-			for (int i = frente; i < tras; i++) {
-				posicao = i % tamanho;
-				System.out.print("[" + sequencia + "]");
-				fila[posicao].imprimir();
-				sequencia++;
-			}
-		}
-	}
-	
-	public double obterMediaGols() {
-		double mediaTotal = 0;
-		int posicao;
-		int tamanhoArr = 0;
-		
-		if (!filaVazia()) {
-			for (int i = frente; i < tras; i++) {
-				tamanhoArr++;
-				posicao = i % tamanho;
-				mediaTotal += fila[posicao].getPlacarSelecao1() +fila[posicao].getPlacarSelecao2();
-			}
-			mediaTotal = mediaTotal / tamanhoArr;
-		}
-		return Math.round(mediaTotal);
-	}
-}
-
 public class Aplicacao{
 	public static void main(String[] args) throws Exception {
 		MyIO.setCharset("UTF-8");
-		Jogo desenfileirado = new Jogo();
 		Jogo[] vetor = Jogo.lerArquivo();
+		Jogo desenfileirado = new Jogo();
+		String pesquisa = MyIO.readLine();
 		Fila fila = new Fila();
-		String s = MyIO.readLine();
 		
-		while(!s.equals("FIM")) {
-			s = MyIO.readLine();
-			String data = s.split(";")[0];
-			String selecao1 = s.split(";")[1];
+		while(!pesquisa.equals("FIM")) {
+			String data = pesquisa.split(";")[0];
+			String selecao1 = pesquisa.split(";")[1];
 			int dia = Integer.parseInt(data.split("/")[0]);
 			int mes = Integer.parseInt(data.split("/")[1]);
 			int ano = Integer.parseInt(data.split("/")[2]);
 			for(int i = 0; i < vetor.length; i++) {
-				if(vetor[i].getDia() == dia && vetor[i].getMes() == mes && vetor[i].getAno() == ano && vetor[i].getSelecao1().equals(selecao1)) {
-					desenfileirado=vetor[i];
-					fila.enfileirar(desenfileirado);
-					break;
+				if(dia == vetor[i].getDia() && mes == vetor[i].getMes() && ano == vetor[i].getAno()
+				&& selecao1.equals(vetor[i].getSelecao1())) {
+					fila.enfileirar((vetor[i]));
+				}
+				
+			}
+			pesquisa = MyIO.readLine();
+		}
+
+		int numFila = Integer.parseInt(MyIO.readLine());
+
+		for (int i = 0; i < numFila; i++) {
+			String EouD = MyIO.readLine();
+			desenfileirado = new Jogo();
+			
+			if (EouD.equals("D")) {
+				desenfileirado = fila.desenfileirar();
+				fila.mostrar(desenfileirado);
+			}else {
+				EouD = EouD.replace("E ", "");
+				String date = EouD.split(";")[0];
+				String Selecao1 = EouD.split(";")[1];
+				int day = Integer.parseInt(date.split("/")[0]);
+				int month = Integer.parseInt(date.split("/")[1]);
+				int year = Integer.parseInt(date.split("/")[2]);
+
+				for (int z = 0; z < vetor.length; z++) {
+					if (day == vetor[z].getDia() && month == vetor[z].getMes()
+							&& year == vetor[z].getAno() && Selecao1.equals(vetor[z].getSelecao1())) {
+						desenfileirado = vetor[z];
+						fila.enfileirar(desenfileirado);
+					}
 				}
 			}
 		}
-		//System.out.println("qntd");
-		int numFila = Integer.parseInt(MyIO.readLine());
-		
-		for(int i = 0; i < numFila; i++) {
-	//System.out.println("EouD");
-	String EouD = MyIO.readLine();
-	desenfileirado = new Jogo();
-	
-	if (EouD.equals("D")) {
-
-		desenfileirado = fila.desenfileirar();
-		fila.mostrar(desenfileirado);
-
-	}
-
-	else {
-		EouD = EouD.replace("E ", "");
-		String date = EouD.split(";")[0];
-		String Selecao1 = EouD.split(";")[1];
-		int day = Integer.parseInt(date.split("/")[0]);
-		int month = Integer.parseInt(date.split("/")[1]);
-		int year = Integer.parseInt(date.split("/")[2]);
-
-		for (int j = 0; j < vetor.length; j++) {
-
-			if (vetor[j].getDia() == day && vetor[j].getMes() == month && vetor[j].getAno() == year && vetor[j].getSelecao1().equals(Selecao1)) {
-
-				desenfileirado = vetor[j];
-
-				fila.enfileirar(desenfileirado);
-
-			}
-
-		}
-
-	}
-		}
-		
-		fila.mostrarJogo();
-		
+		fila.mostrarJogos();
 	}
 }
